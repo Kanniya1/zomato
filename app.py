120ecc4e8baed4b37ca5ca45964c5b3940b5b7fa
Kanniya1 <kanniyadevi182005s@gmail.com>
2026-06-18 23:48:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 st.title('zomato')
-st.write('dashboard starter project')
+st.write("dashboard starter project")
 page=st.sidebar.selectbox("menu",["home","customer","sql analytics"])
 st.write('selected:',page)
